ac4d3c2f707c7b4f4274a8b57bdb0d3acbd9330d
Daniel Bilbao <bilbaod12@gmail.com>
2020-09-09 18:25:26 +0200

    Actualizacion Tarea 22 - Resuelta, falta explicar el README

--- Tarea_22/Tarea_22/main.py
+++ Tarea_22/Tarea_22/main.py
@@ -51,7 +51,7 @@ def CalculoDeProduccion(ListaVacas, CapacidadCamion, TotalProduccion, TotalPesoC
         -TotalProduccion: Cantidad de leche que se produce por dia en caso de llevarse el numero de vacas que se han analizado hasta el momento, su valor va cambiando
         -TotalPesoCamion: Cantidad de peso que lleva el camion en caso de llevarse el numero de vacas que se han analizadno hasta el momento, su valor va cambiando
     Return:
-        -
+        - La produccion maxima que se puede obtener
     """
     if(indice<len(ListaVacas)): #Si todabia no hemos terminado de ver todas las posibilidades..
         VacaPosible=ListaVacas[indice] #Añado a la variable "VacaPosible" un objeto de la lista de vacas disponibles
@@ -59,10 +59,22 @@ def CalculoDeProduccion(ListaVacas, CapacidadCamion, TotalProduccion, TotalPesoC
             #Opcion no valida, llamamos de nuevo a la funcion pero analizando la siguiente vaca
             return CalculoDeProduccion(ListaVacas,CapacidadCamion,TotalProduccion,TotalPesoCamion,indice+1)
         else:
-            #Opcion valida, llamamos de nuevo a la funcion pero analizando la siguiente vaca teniendo en cuenta que cojemos la anterior
+            #Opcion valida, llamamos de nuevo a la funcion pero analizando la siguiente vaca teniendo en cuenta que cojemos la anterior y cojemos el resultado que de mas produccion de leche (con la anterior y sin la anterior)
             return max(CalculoDeProduccion(ListaVacas,CapacidadCamion,TotalProduccion+VacaPosible.produccion,TotalPesoCamion+VacaPosible.peso,indice+1),
-                               CalculoDeProduccion(ListaVacas,CapacidadCamion,TotalProduccion,TotalPesoCamion,indice+1)) #ME FALLA NONETYPE ERROR...!!!!
-        return float(TotalProduccion)
+                               CalculoDeProduccion(ListaVacas,CapacidadCamion,TotalProduccion,TotalPesoCamion,indice+1))
+    return TotalProduccion
+
+def RespuestaCorrecta(x):
+    """Funcion para comprobar que la respuesta del usuario es SI o NO
+    Parametros:
+        -x= String
+    Return:
+        -Devuelve la respuesta SI o NO
+    """
+    while x.upper()!="NO" and x.upper()!="SI" :
+        print("RESPUESTA INCORRECTA, RESPONDA CON UN SI O UN NO")
+        x=input("¿Desea realizar otro analisis con vacas diferentes?")
+    return x
 
 def main():
     VacasEnVenta=[]
@@ -78,7 +90,10 @@ def main():
         VacasEnVenta.append(VacaRegistrada)
     MostrarVacasEnVenta(VacasEnVenta)
     ProduccionMaxima=CalculoDeProduccion(VacasEnVenta,PesoCamion,0,0,0)
-    print(ProduccionMaxima)
+    print("\nLa producion maxima que se puede obtener es de: "+str(ProduccionMaxima)+" Litros")
 #endregion
 
-main()
+respuesta=""
+while respuesta.upper()!="NO":
+    main()
+    respuesta=RespuestaCorrecta(input("¿Desea realizar otro analisis con vacas diferentes?"))
