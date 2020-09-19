517f36cab80a45a65fddc5da929cb99938cc3505
Daniel Bilbao <bilbaod12@gmail.com>
2020-09-19 17:45:24 +0200

    Actualizacion Tarea23 - Ristra generada

--- Tarea_23/Tarea_23/Tarea_23.py
+++ Tarea_23/Tarea_23/Tarea_23.py
@@ -3,6 +3,8 @@
 
 from Baraja import *
 
+#Variable global para la conversion de NUMERO-LETRA
+diccionario={1:"a",2:"b",3:"c",4:"d",5:"e",6:"f",7:"g",8:"h",9:"i",10:"j",11:"k",12:"l",13:"m",14:"n",15:"o",16:"p",17:"q",18:"r",19:"s",20:"t",21:"u",22:"v",23:"w",24:"x",25:"y",26:"z"}
 
 #region
 def DividirCadena(cadena, separador, numeroCaracteres):
@@ -84,6 +86,74 @@ def CortarEntreComodines(baraja):
     print("\nLa baraja tras el corte entre comodines queda asi:")
     MostrarBaraja(baraja)
 
+def CortarMirandoUltimaCarta(baraja):
+    UltimaCarta=baraja.mazo[53]
+    if UltimaCarta.palo=="Comodin": #Si la ultima carta es comodin, se deja la baraja igual
+        print("\nComo la ultima carta es comodin, la baraja no cambia")
+    else:
+        ValorUltimaCarta=UltimaCarta.valor
+        print("\nLa ultima carta del mazo es un "+str(UltimaCarta)+" por lo que su valor es de "+str(ValorUltimaCarta))
+        ValorNumericoUltimaCarta=ConversionValorNumero(ValorUltimaCarta)
+        PaloUltimaCarta=UltimaCarta.palo
+        if PaloUltimaCarta=="Treboles":
+            ValorAContar=ValorNumericoUltimaCarta
+        elif PaloUltimaCarta=="Diamantes":
+            ValorAContar=ValorNumericoUltimaCarta+13
+        elif PaloUltimaCarta=="Corazones":
+            ValorAContar=ValorNumericoUltimaCarta+26
+        elif PaloUltimaCarta=="Picas":
+            ValorAContar=ValorNumericoUltimaCarta+39
+        PrimerTrozo=baraja.mazo[0:ValorAContar]
+        TrozoMedio=baraja.mazo[ValorAContar:len(baraja.mazo)-1]
+        SegundoTrozo=baraja.mazo[len(baraja.mazo)-1:]
+        baraja.mazo=TrozoMedio+PrimerTrozo+SegundoTrozo
+        MostrarBaraja(baraja)
+
+def ObtenerCaracter(baraja):
+    PrimeraCarta=baraja.mazo[0]
+    ValorNumericoPrimeraCarta=ConversionValorNumero(PrimeraCarta.valor)
+    if PrimeraCarta.palo=="Treboles":
+        ValorAContar=ValorNumericoPrimeraCarta+0
+    elif PrimeraCarta.palo=="Diamantes":
+        ValorAContar=ValorNumericoPrimeraCarta+13
+    elif PrimeraCarta.palo=="Corazones":
+        ValorAContar=ValorNumericoPrimeraCarta+26
+    elif PrimeraCarta.palo=="Picas":
+        ValorAContar=ValorNumericoPrimeraCarta+39
+    elif PrimeraCarta.palo=="Comodin": #Si es comodin se cuenta 53
+        ValorAContar=53
+    CartaCaracter=baraja.mazo[ValorAContar] 
+    return CartaCaracter
+
+def ConversionValorNumero(x):
+    if x=="A":
+        valor=1
+    elif x=="2":
+        valor=2
+    elif x=="3":
+        valor=3
+    elif x=="4":
+        valor=4
+    elif x=="5":
+        valor=5
+    elif x=="6":
+        valor=6
+    elif x=="7":
+        valor=7
+    elif x=="8":
+        valor=8
+    elif x=="9":
+        valor=9
+    elif x=="10":
+        valor=10
+    elif x=="J":
+        valor=11
+    elif x=="Q":
+        valor=12
+    elif x=="K":
+        valor=13
+    return valor
+
 def Cifrado1 (mensaje): #Primer paso del cifrado, divide la frase en grupos de 5 letras y si hacen falta caracteres se le añade 'X' al final
     """Funcion que hace el primer paso para el cifrado
     Parametros:
@@ -97,6 +167,17 @@ def Cifrado1 (mensaje): #Primer paso del cifrado, divide la frase en grupos de 5
     mensaje=DividirCadena(mensaje," ",5) #Separo el string en cadenas de 5 caracteres
     return mensaje
 
+def Cifrado2 (mensaje,baraja):
+    ristra=""
+    while len(mensaje)>len(ristra): #Mientras que la dimension de la ristra sea menor que la del mensaje a cifrar, generamos caracteres (Solitario)
+        for letra in mensaje:
+                if letra!=" ":
+                    ristra=ristra+Solitario(baraja)
+                else:
+                    ristra=ristra+" "
+    print("\nLa ristra generada es la siguiente: "+str(ristra))
+    return ristra
+
 def Solitario (baraja):
     #Encontrar comodinA y moverlo debajo de la carta que tiene debajo
     [IndiceComodinA,IndiceComodinB]=BuscarComodines(baraja.mazo)
@@ -108,27 +189,39 @@ def Solitario (baraja):
     MoverComodinB(IndiceComodinB,baraja)
     #Corto la baraja e intercambio las cartas encima del primer comodin por las de debajo del segundo comodin
     CortarEntreComodines(baraja)
-    #Miro la ultima carta y cuento el numero que sea desde arriba--> si la ultima carta es comodin, se deja la baraja igual
-    UltimaCarta=baraja.mazo[53]
-    ValorUltimaCarta=UltimaCarta.valor
-    print("\nLa ultima carta del mazo es un "+str(UltimaCarta)+" por lo que su valor es de "+str(ValorUltimaCarta))
-    #AHORA TENGO QUE HACER LA JUGADA DE SUMAR 13 A LOS DIAMANTES, 26 A LOS CORAZONES Y 39 A LAS PICAS
+    #Miro la ultima carta y cuento el numero que sea desde arriba
     #Una vez contado, corto la baraja dejando la carta de la ultima posicion tal y como esta
-    #Observo la primera carta, cuento, y almaceno que carta es.--> Esta carta se convierte a letra y ya tenemos el primer caracter
+    CortarMirandoUltimaCarta(baraja)
+    #Observo la primera carta, cuento, y almaceno que carta es
+    global ValorLetra
+    Caracter=ObtenerCaracter(baraja)
+    if Caracter.palo=="Comodin":
+        Solitario(baraja) #APLICO RECURSIVIDAD
+    #Convertir el caracter a letra, y ya esta "solitario"
+    if Caracter.palo=="Diamantes" or Caracter.palo=="Picas":
+        ValorLetra=ConversionValorNumero(Caracter.valor)+13
+    elif Caracter.palo=="Treboles" or Caracter.palo=="Corazones":
+        ValorLetra=ConversionValorNumero(Caracter.valor)+0
+    for k,i in diccionario.items():
+        if k==ValorLetra:
+            Letra=diccionario[k]
+    return Letra.upper()
 
 def main():
     frase=input("Introduzca una frase a cifrar: ") #Esta es la frase que tenemos que cifrar
+    #PRIMER PASO DEL CIFRADO
     frase=Cifrado1(frase)
     print(frase)
     baraja1=GenerarBaraja()
     print("\nLa baraja inicialmente esta asi:") #CUANTO TERMINE DE PROGRAMAR EL ALGORITMO CON ESTA CLAVE QUE HE DEFINIDO, MODIFICAR EL PROGRAMA PARA PEDIR CLAVE AL USUARIO Y BARAJAR RESPECTO A ESA CLAVE
     MostrarBaraja(baraja1)
-    #ristra=""
-    #while len(frase)>len(ristra): #Mientras que la dimension de la ristra sea menor que la del mensaje a cifrar, generamos caracteres (Solitario)
-    Solitario(baraja1)
+    #SEGUNDO PASO DEL CIFRADO
+    Ristra=Cifrado2(frase,baraja1)
+    #TERCER PASO DEL CIFRADO--> SUMAR FRASE Y RISTRA
+    #PRIMERO: CONVERTIR FRASE A NUMEROS
 
-
-
+    #SEGUNDO: CONVERTIR RITRA A NUMEROS
+    #TERCERO: SUMARLO Y CONVERTIR EL RESULTADO DE CADA SUMA A LETRA, JUNTARLAS EN UNA FRASE, Y TEXTO CIFRADO
 #endregion
 
 main()
