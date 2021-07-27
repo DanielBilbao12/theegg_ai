6300e679ba990c28d2f038c2300f43d4f3954086
Daniel <bilbaod12@gmail.com>
2021-07-27 16:28:11 +0200

    Falta README.md

--- tarea_52/tarea_52/Ejercicio2.py
+++ tarea_52/tarea_52/Ejercicio2.py
@@ -8,9 +8,64 @@
 #- Informar qué nombres de nivel primario no se repiten en los de nivel secundario.
 
 #region
+def SolicitarNombres(string):
+    """Funcion que solcita nombres al usuario hasta que este introduce "x"
+    Return:
+        -Devuelve una lista con los nombres introducidos por el usuario
+    """
+    lista=[]
+    print("A continuacion, rellena la lista de los alumnos de "+str(string)+":")
+    nombre=input("Introduzca un nombre:")
+    while nombre!="x":
+        lista.append(nombre.upper())
+        nombre=input("Introduzca un nombre:")
+    return lista
+
+def MostrarTodosLosNombres(listap, listas):
+    """Funcion que muestra todos los nombres de los alumnos de primaria y secundaria sin repeticiones
+    Parametros:
+        -listap: Lista de nombres de alumnos de primaria
+        -listas: Lista de nombres de alumnos de secundaria
+    Return:
+        -Muestra por pantalla todos los nombres de ambos cursos sin repeticiones
+    """
+    print("A continuacion, se muestran todos los nombres de ambos cursos, sin repeticiones:")
+    for nombre in listap|listas:
+        print(nombre)
+
+def MostrarRepeticionesEntreCursos(listap, listas):
+    """Funcion que muestra todos los nombres repetidos entre ambos cursos
+    Parametros:
+        -listap: Lista de nombres de alumnos de primaria
+        -listas: Lista de nombres de alumnos de secundaria
+    Return:
+        -Muestra por pantalla los nombres que se repiten entre ambos cursos
+    """
+    print("A continuacion, se muestran los nombres repetidos entre ambos cursos:")
+    for nombre in listap&listas:
+        print(nombre)
+
+def MostrarNoRepeticionesEntreCursos(listap, listas):
+    """Funcion que muestra todos los nombres no repetidos entre ambos cursos
+    Parametros:
+        -listap: Lista de nombres de alumnos de primaria
+        -listas: Lista de nombres de alumnos de secundaria
+    Return:
+        -Muestra por pantalla los nombres que no se repiten entre ambos cursos
+    """
+    print("A continuacion, se muestran los nombres de primaria no repetidos en secundaria:")
+    for nombre in listap-listas: #Si se querrian los de nivel secundario, restar alreves (listas-listap) ya que si los elementos no son iguales, el resultado almacena el elemento del primer objeto
+        print(nombre)
+
 
 def main():
     """Programa principal"""
+    ListaPrimaria=set(SolicitarNombres("primaria"))
+    ListaSecundaria=set(SolicitarNombres("secundaria"))
+    MostrarTodosLosNombres(ListaPrimaria, ListaSecundaria)
+    MostrarRepeticionesEntreCursos(ListaPrimaria, ListaSecundaria)
+    MostrarNoRepeticionesEntreCursos(ListaPrimaria, ListaSecundaria)
+
 
 #endregion
 
